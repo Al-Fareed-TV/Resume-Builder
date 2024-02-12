8a726beacdaf8142a15733b1f8ee7839fe9d4e74
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2024-02-12 12:02:06 +0530

    inital project setup

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# resume-builder
+A resume builder for TestVagrant Employees
---
+++ src/builder/ProfileBuider.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfileBuider = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProfileBuider
