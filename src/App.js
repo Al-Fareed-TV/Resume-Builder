7101588e1c99c94e520e6159ee4f39b6e25ab39d
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2024-02-14 22:07:59 +0530

    added TV Spinner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import React, { useState, useEffect } from "react";
 import DownloadPDF from "./Page/DownloadPDF";
 import MyPDF from "./Page/MyPDF";
-
+import LoadingPage from "./Page/LoadingPage";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-         <MyPDF/>
-         <DownloadPDF/>
+      {loading && <LoadingPage />}
+      {!loading && <MyPDF />}
+      {!loading && <DownloadPDF />}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Page/LoadingPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./LoadingPage.css"
+const LoadingPage = () => {
+  return (
+    <div className="loading-container">
+      <div class="circle1">
+        <div class="circle2">
+          <div class="circle3"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingPage;
